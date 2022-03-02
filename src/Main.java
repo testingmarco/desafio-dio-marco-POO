ac686504bf2ac476a22d97e00c1c40592bd9a5a5
Marco Auelio <testingmarco@outlook.com>
2022-03-02 20:42:08 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import br.com.dio.desfio.dominio.Curso;
+import br.com.dio.desfio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricão curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricão curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDecricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
