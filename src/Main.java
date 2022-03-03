79ddde2047cc7e01178636f6fd4d553c805ddfaf
Marco Auelio <testingmarco@outlook.com>
2022-03-03 00:15:13 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desfio.dominio.Curso;
-import br.com.dio.desfio.dominio.Mentoria;
+import br.com.dio.desfio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,9 +20,39 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        //System.out.println(curso1);
+        //System.out.println(curso2);
+        //System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMarco = new Dev();
+        devMarco.setNome("Marco");
+        devMarco.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Marco" + devMarco.getConteudosInscritos());
+        devMarco.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Marco" + devMarco.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Marco" + devMarco.getConteudosConcluidos());
+        System.out.println("XP" + devMarco.calcularTotalXp());
+
+
+        System.out.println("-------");
+
+        Dev devJosivanja = new Dev();
+        devJosivanja.setNome("Josivanja");
+        devJosivanja.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Josivanja" + devJosivanja.getConteudosInscritos());
+        devJosivanja.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Josivanja" + devJosivanja.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Josivanja" + devJosivanja.getConteudosConcluidos());
+        System.out.println("XP" + devJosivanja.calcularTotalXp());
 
     }
 }
